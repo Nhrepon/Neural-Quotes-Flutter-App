1b0a70bd5df97fb13f94390817f93af2fb67f9ef
nhrepon <nurhossainrepon7248@gmail.com>
2024-11-06 21:16:30 +0600

    version: 1.0.3

--- lib/main.dart
+++ lib/main.dart
@@ -1,71 +1,101 @@
 import 'package:flutter/material.dart';
 
-
-
 void main() {
   runApp(const NeuralQuoutes());
 }
 
-
-
 class NeuralQuoutes extends StatelessWidget {
   const NeuralQuoutes({super.key});
   final String title = "Neural Quotes";
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: title,
-        home: Scaffold(
-
-
-          appBar: AppBar(
-            title: Text(title),
-            centerTitle: true,
-            backgroundColor: Colors.green,
-            leading: Icon(Icons.menu),
-            actions: <Widget>[
-              IconButton(
-                icon: Icon(Icons.verified_user_sharp),
-                tooltip: "Appbar actions",
-                onPressed: (){
-
-                },
-              )
-            ],
-          ),
-
-
-
-
-
-          body: Center(
-            child: Text("data", style: TextStyle(fontSize: 32),),
-          ),
-
-
-
-
-
-          bottomNavigationBar: NavigationBar(
-            backgroundColor: Colors.green,
-            destinations: [
-              NavigationDestination(icon: Icon(Icons.home), label: "Home"),
-              NavigationDestination(icon: Icon(Icons.trending_down), label: "Trending"),
-              NavigationDestination(icon: Icon(Icons.bookmark), label: "Bookmark"),
-            ],
-
-
-          ),
-
-
-
-
-
-        )
-
+      title: title,
+      home: Home(),
     );
   }
 }
+
+class Home extends StatelessWidget {
+  Home({super.key});
+  final String title = "Neural Quotes";
+  List<String> dataList = [
+    "Nur Hossain ",
+    "Repon",
+    "Sharmin akter",
+    "Sharmin",
+    "Sharmin",
+    "Sharmin",
+    "Repon",
+    "Sharmin akter",
+    "Repon",
+    "Sharmin akter",
+    "Sharmin akter",
+    "Sharmin akter",
+    "Repon",
+    "Repon",
+    "Repon",
+    "Sharmin akter",
+    "Sharmin akter",
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+        centerTitle: true,
+        backgroundColor: Colors.green,
+        //leading: Icon(Icons.menu),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.verified_user_sharp),
+            tooltip: "Appbar actions",
+            onPressed: () {},
+          )
+        ],
+      ),
+
+
+      drawer: Drawer(
+        width: 300,
+        shadowColor: Colors.black,
+        child: Column(
+          children: [Text("Drawer title")],
+        ),
+      ),
+
+
+
+      body: Scrollbar(
+        child: ListView.builder(
+            padding: EdgeInsets.symmetric(vertical: 20, horizontal: 30),
+            itemCount: dataList.length,
+            itemBuilder: (context, index) {
+              return Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(dataList[index]),
+              );
+            }),
+      ),
+
+
+
+
+      bottomNavigationBar: NavigationBar(
+        backgroundColor: Colors.green,
+        destinations: [
+          NavigationDestination(icon: Icon(Icons.home), label: "Home"),
+          NavigationDestination(
+              icon: Icon(Icons.trending_down), label: "Trending"),
+          NavigationDestination(icon: Icon(Icons.bookmark), label: "Bookmark"),
+        ],
+      ),
+    );
+  }
+}
+
 
 
 
